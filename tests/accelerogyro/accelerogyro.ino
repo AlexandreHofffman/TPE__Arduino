cb446892ebca0fdc30f5bc10c9933952ef0a982c
TahitoaL <36546314+TahitoaL@users.noreply.github.com>
2018-02-16 20:14:12 +0100

    Création fichiers test
    Création des fichiers test pour le transistor et l'accéléromètre

---
+++ tests/accelerogyro/accelerogyro.ino
@@ -0,0 +1,88 @@
+class hysteresis
+{
+	private:
+		int savedValue;
+		boolean currentState;
+		int seuilHaut;
+		int seuilBas;
+	public:
+		hysteresis();
+		void setUp(int aSeuilBas,  int aSeuilHaut);
+		void setValue(int value);
+		int getState();
+};
+
+hysteresis::hysteresis()
+{
+	
+}
+
+void hysteresis::setUp(int aSeuilBas,  int aSeuilHaut)
+{
+	seuilHaut = aSeuilHaut;
+	seuilBas = aSeuilBas;
+	currentState = false;
+}
+
+void hysteresis::setValue(int value)
+{
+	savedValue = value;
+}
+
+int hysteresis::getState()
+{
+	if (savedValue > seuilHaut && !currentState)
+	{
+		currentState = true;
+	}
+	if (savedValue < seuilBas && currentState)
+	{
+		currentState = false;
+	}
+
+	return int(currentState);
+}
+
+hysteresis arretY()
+
+#include<Wire.h>
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  arretY(580, 620);
+  Serial.begin(9600);
+}
+void loop(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  /*Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);*/
+  arretY.setValue(AcY);
+  if (arretY.getState() == 1)
+  {
+  	digitalWrite(4, HIGH);
+  }
+  else
+  {
+  	digitalWrite(4, LOW);
+  }
+}
---
+++ tests/transistors/transistors.ino
@@ -0,0 +1,13 @@
+byte val;
+
+void setup()
+{
+	pinMode(3, OUTPUT);
+}
+
+void loop()
+{
+	val = analogRead(A0) / 4;
+	analogWrite(3, val);
+	delay(50);
+}
