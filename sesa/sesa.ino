74339863cdee7cf7fff47789284d300495946db5
TahitoaL <36546314+TahitoaL@users.noreply.github.com>
2018-02-20 18:05:07 +0100

    Retour normal

--- sesa/sesa.ino
+++ sesa/sesa.ino
@@ -401,12 +401,6 @@ float currentVitesse;
 float distanceRoue;
 boolean blinkOn = false;
 
-// INTEGRATION ACCELOROMETRE.begin
-#include<Wire.h>
-const int MPU_addr=0x68;  // I2C address of the MPU-6050
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
-// INTEGRATION ACCELOROMETRE.end
-
 lampe ledRouge(11);
 lampe ledBlanc(10);
 binaryLampe ledStop(9);
@@ -435,13 +429,6 @@ void setup()
 	arretY(580, 620);
 	vitesse0 = false;
 	blinkOn = false;
-	//Accelero.setUp.begin
-	Wire.begin();
-  	Wire.beginTransmission(MPU_addr);
-	Wire.write(0x6B);  // PWR_MGMT_1 register
-	Wire.write(0);     // set to zero (wakes up the MPU-6050)
- 	Wire.endTransmission(true);
- 	//Accelero.setUp.end
 	Serial.begin(9600);
 	Serial.println("Fin du setUp !");
 }
@@ -458,24 +445,6 @@ void loop()
 	aimantCounter = 0;
 	while(tempsVitesse.timeIsUp() == 0 && !stop)
 	{
-		//Accelero.loop.begin
-		if (tempsAccelero.timeIsUp() == 1)
-		{
-			Wire.beginTransmission(MPU_addr);
-			Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-			Wire.endTransmission(false);
-			Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-			AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-			AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-			AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-			Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-			GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-			GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-			GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-			arretY.setValue(AcY);
-		}
-		//Accelero.loop.end
-
 		aimantVitesse.setPreviousState();
 		photoSensor.setPreviousState();
 		frein.setPreviousState();
