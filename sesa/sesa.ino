5f4c32134969e4e0d5cb307c2d847ce0aee92fc7
TahitoaL <36546314+TahitoaL@users.noreply.github.com>
2018-02-20 18:04:05 +0100

    Intégration accéléro

--- sesa/sesa.ino
+++ sesa/sesa.ino
@@ -398,6 +398,10 @@ int axe::getState()
   }
 }
 
+#include <Wire.h>
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
 const unsigned int timerValue = 5000;
 const float diametreRoue = 31.85;
 boolean serialDebug = true;
@@ -426,12 +430,19 @@ timer tempsVitesse(timerValue); //Temps defini pour la mesure de vitesse
 timer tempsFrein(2000); // Minuteur permettant de dire que le vélo est à l'arrêt si aucun aimant n'est passé devant le capeteur pendant plus de 2 secondes
 timer blink(330); // Minuteur pour le clignotement des lumières
 timer tempsAccelero(10);
-axe axeX(1400, 1600, 10);
-axe axeY(1400, 1600, 10);
-axe axeZ(800, 1000, 10);
+axe axeX(1400, 1600, 15);
+axe axeY(1400, 1600, 15);
+axe axeZ(800, 1000, 15);
 
 void setup()
 {
+	//AcceleroSetup.begin
+	  Wire.begin();
+	  Wire.beginTransmission(MPU_addr);
+	  Wire.write(0x6B);  // PWR_MGMT_1 register
+	  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+	  Wire.endTransmission(true);
+	//AcceleroSetup.end
 	ledRouge.setUp();
 	ledBlanc.setUp();
 	ledStop.setUp();
@@ -459,10 +470,26 @@ void loop()
 	aimantCounter = 0;
 	while(tempsVitesse.timeIsUp() == 0 && !stop)
 	{
+		//AcceleroRead.begin
 		if (tempsAccelero.timeIsUp() == 1)
 		{
-		    
+		  Wire.beginTransmission(MPU_addr);
+		  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+		  Wire.endTransmission(false);
+		  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+		  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+		  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+		  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+		  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+		  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+		  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+		  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 		}
+		//AcceleroRead.end
+		axeX.setValue(AcX);
+		axeY.setValue(AcY);
+		axeZ.setValue(AcZ);
+
 		aimantVitesse.setPreviousState();
 		photoSensor.setPreviousState();
 		frein.setPreviousState();
@@ -470,7 +497,7 @@ void loop()
 		savedState = aimantVitesse.getState();
 		savedValue = photoSensor.getState();
 		savedStateFrein = frein.getState();
-		if (tempsFrein.timeIsUp() == 1)
+		if (tempsFrein.timeIsUp() == 1) //|| axeX.getState() == 0 || axeY.getState() == 0 || axeZ.getState() == 0)
 		{
 			Serial.println("Le velo est a l'ARRET !");
 			stop = true;
